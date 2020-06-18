206f954277028f971d2ee5657fc3d681466e3a3e
Francis Wang <frwang@mit.edu>
2020-06-18 16:36:19 +0800

    Support for memory XBar

---
+++ mappings/CoherentXBar_xbar.py
@@ -0,0 +1,21 @@
+gem5_class = "CoherentXBar"
+accelergy_class = "xbar"
+path = "system.chip"
+name_append = ""
+
+criteria = True
+
+constants = [
+    ("horizontal_nodes", 1),
+    ("vertical_nodes", 1),
+    ("link_throughput", 1)
+]
+
+attributes = [
+    ("link_latency", "response_latency"),
+    ("flit_bytes", "width")
+]
+
+actions = [
+    ("access", "pkt_count::total")
+]
